81113868862ce1405794b3205c2155f971e5f855
Rui Guo <ruiguo.developer@outlook.com>
2023-09-04 08:55:38 +1000

    Finished tutorial

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -7,24 +7,27 @@
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
+    document.getElementById("insert-paragraph").onclick = () => tryCatch(insertParagraph);
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
   }
 });
 
-export async function run() {
-  return Word.run(async (context) => {
-    /**
-     * Insert your Word code here
-     */
-
-    // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
-
-    // change the paragraph color to blue.
-    paragraph.font.color = "blue";
-
+async function insertParagraph() {
+  await Word.run(async (context) => {
+    const docBody = context.document.body;
+    docBody.insertParagraph(
+      "Office has several versions, including Office 2016, Microsoft 365 Apps, and Office on the web.",
+      Word.InsertLocation.start
+    );
     await context.sync();
   });
+}
+
+async function tryCatch(callback) {
+  try {
+    await callback();
+  } catch (error) {
+    Console.error(error);
+  }
 }
