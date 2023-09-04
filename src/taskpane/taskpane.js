51cf19d5f407d4cf1da6f5fdcf3f791f56257941
Rui Guo <ruiguo.developer@outlook.com>
2023-09-04 17:19:46 +1000

    Change text

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -11,6 +11,9 @@ Office.onReady((info) => {
     document.getElementById("apply-style").onclick = () => tryCatch(applyStyle);
     document.getElementById("apply-custom-style").onclick = () => tryCatch(applyCustomStyle);
     document.getElementById("change-font").onclick = () => tryCatch(changeFont);
+    document.getElementById("insert-text-into-range").onclick = () => tryCatch(insertTextIntoRange);
+    document.getElementById("insert-text-outside-range").onclick = () => tryCatch(insertTextOutsideRange);
+    document.getElementById("replace-text").onclick = () => tryCatch(replaceText);
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
   }
@@ -53,12 +56,45 @@ async function applyCustomStyle() {
 
 async function changeFont() {
   await Word.run(async (context) => {
-    const sencondParagraph = context.document.body.paragraphs.getNext();
+    const sencondParagraph = context.document.body.paragraphs.getFirst().getNext();
     sencondParagraph.font.set({
       name: "Courier New",
       bold: true,
       size: 18,
     });
+    await context.sync();
+  });
+}
+
+async function insertTextIntoRange() {
+  await Word.run(async (context) => {
+    const doc = context.document;
+    const originalRange = doc.getSelection();
+    originalRange.insertText(" (M365)", Word.InsertLocation.end);
+    originalRange.load("text");
+    await context.sync();
+    doc.body.insertParagraph("Original range: " + originalRange.text, Word.InsertLocation.end);
+    await context.sync();
+  });
+}
+
+async function insertTextOutsideRange() {
+  await Word.run(async (context) => {
+    const doc = context.document;
+    const originalRange = doc.getSelection();
+    originalRange.insertText("Office 2019 ", Word.InsertLocation.before);
+    originalRange.load("text");
+    await context.sync();
+    doc.body.insertParagraph("Current text of original range: " + originalRange.text, Word.InsertLocation.end);
+    await context.sync();
+  });
+}
+
+async function replaceText() {
+  await Word.run(async (context) => {
+    const doc = context.document;
+    const originalRange = doc.getSelection();
+    originalRange.insertText("many", Word.InsertLocation.replace);
     await context.sync();
   });
 }
